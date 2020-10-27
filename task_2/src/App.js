32c2b160370937d00a9805ff08e7ee6de2b9abea
Alabbey2 <47299377+Alabbey2@users.noreply.github.com>
2020-10-27 23:44:50 +0200

    [Update App.js] - added a functional component to display h1 element 'Hello World'.

--- task_2/src/App.js
+++ task_2/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  document.title="React with Margit";
   return (
     <div className="App">
-     <h1>na me</h1>
+     <h1>Hello World!</h1>
+     
     </div>
+    
   );
 }
 
