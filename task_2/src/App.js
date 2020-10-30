b351da18579cab6a98d373152ffa2bb8c517451b
Alabbey2 <47299377+Alabbey2@users.noreply.github.com>
2020-10-30 10:20:54 +0200

    changes

--- task_2/src/App.js
+++ task_2/src/App.js
@@ -1,17 +1,41 @@
-
-//import logo from './logo.svg';
+import React, {Component} from "react";
 import './App.css';
-import Animal from './Animal/Animal';
+import Animal from './Component/Animal/Animal';
 
-function App() {
-  document.title="React with Margit";
-  return (
-    <div className="App">
-     
-     <Animal />
-    </div>
+
+class App extends Component {
+  state = {
+  animals: [
+    { name: "Lion", img:"https://source.unsplash.com/200x200/?lion"},
+     {name: "Tiger", img:"https://source.unsplash.com/200x200/?tiger"},
+    { name: "Dog", img:"https://source.unsplash.com/200x200/?dog"},
+  ],
+};
+
+
+  render () {
+    return (
+      <div className="cards">
     
-  );
+      <Animal 
+      name={this.state.animals[0].name} 
+      img={this.state.animals[0].img} 
+      />
+      <Animal 
+      name={this.state.animals[1].name} 
+      img={this.state.animals[1].img} 
+      />
+      <Animal 
+      name={this.state.animals[2].name} 
+      img={this.state.animals[2].img}  
+      />
+      
+     </div>
+     );
+  }
 }
+
+
+
 
 export default App;
