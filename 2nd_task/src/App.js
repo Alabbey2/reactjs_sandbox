f8ae99cad596c5df3a5cc870a6a043a89d95b94f
Alabbey2 <47299377+Alabbey2@users.noreply.github.com>
2020-11-01 18:58:29 +0200

    [Animal component state] - added state as a source of compaonent's data

--- 2nd_task/src/App.js
+++ 2nd_task/src/App.js
@@ -3,16 +3,33 @@ import Animal from './Components/Animal';
 import "./App.css";
 
 class App extends Component {
+
+  state = {
+    animals: [
+      { name:"Dog", image:"https://source.unsplash.com/weekly?/dog"},
+      { name:"Lion", image:"https://source.unsplash.com/weekly?/lion"},
+       {name:"Cat", image:"https://source.unsplash.com/weekly?/cat"},
+    ],
+  };
+
   render () {
     return (
       <div className="component-cards">
       
-      <Animal name="Dog" image="https://source.unsplash.com/weekly?/dog" />
+      <Animal 
+      name={this.state.animals[0].name} 
+      image={this.state.animals[0].image}
+      />
 
-      <Animal name="Lion" image="https://source.unsplash.com/weekly?/lion" />
+      <Animal  
+      name={this.state.animals[1].name} 
+      image={this.state.animals[1].image}
+      />
 
-      <Animal name="Cat" image="https://source.unsplash.com/weekly?/cat" />
-
+      <Animal  
+      name={this.state.animals[2].name} 
+      image={this.state.animals[2].image}
+      />
       
     </div>
     );
