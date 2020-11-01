a4071757768301e6a51c84a441a2304ee6aec999
Alabbey2 <47299377+Alabbey2@users.noreply.github.com>
2020-11-01 21:18:29 +0200

    [Counter buttons and state] - added event handler to buttons to handle addition, subtraction and reset events. Use setState to make counting dynamic

--- 3rd_task/src/App.js
+++ 3rd_task/src/App.js
@@ -6,17 +6,30 @@ import './App.css';
 class App extends Component {
   state = {
     likes: 0,
-    
+  };
+
+  addHandler = () => {
+    // console.log('addHandler was clicked');
+    this.setState({likes:this.state.likes + 1});
   }
+  removeHandler = () => {
+    // console.log('removeHandler was clicked');
+    this.setState({likes:this.state.likes - 1});
+  }
+  resetHandler = () => {
+    // console.log('resetHandler was clicked');
+    this.setState({likes: 0});
+  }
+
   render() {
     return (
       
       <div>
         <header />
         <h1 className="likes">Total Likes:{this.state.likes}</h1>
-        <button className="event-button">Add like</button>
-        <button className="event-button">Remove like</button>
-        <button className="event-button">Reset</button>
+        <button onClick={this.addHandler}>Add like</button>
+        <button onClick={this.removeHandler}>Remove like</button>
+        <button onClick={this.resetHandler}>Reset</button>
       </div>
     );
   }
