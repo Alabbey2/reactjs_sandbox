5b5faa11ea77e49617ca065f45bc7446f093929e
Alabbey2 <47299377+Alabbey2@users.noreply.github.com>
2020-11-01 20:33:31 +0200

    [Declaration of state] - added state to App component to declare number of likes

--- 3rd_task/src/App.js
+++ 3rd_task/src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+
 class App extends Component {
+  state = {
+    likes: 0,
+    
+  }
   render() {
     return (
+      
       <div>
-        <h1>Total Likes:</h1>
+        <header />
+        <h1 className="likes">Total Likes:{this.state.likes}</h1>
+        <button className="event-button">Add like</button>
+        <button className="event-button">Remove like</button>
+        <button className="event-button">Reset</button>
       </div>
     );
   }
