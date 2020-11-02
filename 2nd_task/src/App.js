67a0c5435ab97cb0e452858c0865712dbc3a4711
Alabbey2 <47299377+Alabbey2@users.noreply.github.com>
2020-11-02 15:16:01 +0200

    [Map function & Id] - Added card list with map function and Id for each item

--- 2nd_task/src/App.js
+++ 2nd_task/src/App.js
@@ -6,33 +6,41 @@ class App extends Component {
 
   state = {
     animals: [
-      { name:"Dog", image:"https://source.unsplash.com/weekly?/dog"},
-      { name:"Lion", image:"https://source.unsplash.com/weekly?/lion"},
-       {name:"Cat", image:"https://source.unsplash.com/weekly?/cat"},
+      {id:1, name:"Dog", image:"https://source.unsplash.com/weekly?/dog"},
+      {id:2, name:"Lion", image:"https://source.unsplash.com/weekly?/lion"},
+      {id:3, name:"Cat", image:"https://source.unsplash.com/weekly?/cat"},
     ],
   };
 
   render () {
-    return (
-      <div className="component-cards">
+    const animalsList = this.state.animals.map((animal) => {
+      return (
+        <Animal 
+          key={animal.name} 
+          name={animal.name} 
+          image={animal.image} />
+      );
+    });
+    return <div className="component-cards"> {animalsList} </div>;
+    //   <div className="component-cards">
       
-      <Animal 
-      name={this.state.animals[0].name} 
-      image={this.state.animals[0].image}
-      />
+    //   <Animal 
+    //   name={this.state.animals[0].name} 
+    //   image={this.state.animals[0].image}
+    //   />
 
-      <Animal  
-      name={this.state.animals[1].name} 
-      image={this.state.animals[1].image}
-      />
+    //   <Animal  
+    //   name={this.state.animals[1].name} 
+    //   image={this.state.animals[1].image}
+    //   />
 
-      <Animal  
-      name={this.state.animals[2].name} 
-      image={this.state.animals[2].image}
-      />
+    //   <Animal  
+    //   name={this.state.animals[2].name} 
+    //   image={this.state.animals[2].image}
+    //   />
       
-    </div>
-    );
+    // </div>
+    // );
   }
 }
 
