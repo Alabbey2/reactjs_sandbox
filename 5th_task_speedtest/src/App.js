33cabcf453cc9b2fded66e8ea2bbf84c598386f3
Alabbey2 <47299377+Alabbey2@users.noreply.github.com>
2020-11-05 12:49:54 +0200

    added cirle component and styling

--- 5th_task_speedtest/src/App.js
+++ 5th_task_speedtest/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Circle from './Components/Circle/Circle'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello</h1>
+        <h1>Speedgame</h1>
+        <p>Your score is:</p>
+        <button>STart</button>
+        <button>End</button>
       </div>
     );
   }
