aa68772b5a0284ca0dd2cb16f6c531c986a4c990
Alabbey2 <47299377+Alabbey2@users.noreply.github.com>
2020-11-13 13:26:14 +0200

    [Event Handler] - added event handlers to form.js

--- 6th_task_form/src/App.js
+++ 6th_task_form/src/App.js
@@ -3,11 +3,25 @@ import Form from './Components/Form/Form'
 import View from './Components/View/View'
 
 class App extends Component {
+  state = {
+    firstname: "",
+    lastname: "",
+    phone:"",
+    message:"",
+    role:""
+  };
+  
   render() {
     return (
       <div>
         <Form />
-        <View />
+
+        <View 
+        firstname={this.state.firstname} 
+        lastname={this.state.lastname} 
+        phone={this.state.phone} 
+        message={this.state.message} 
+        role={this.state.role}/>
       </div>
     );
   }
