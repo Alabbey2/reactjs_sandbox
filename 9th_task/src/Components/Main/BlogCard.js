3104ec4629416325c09ccbfb0ca2917a8a7c1348
Alabbey2 <47299377+Alabbey2@users.noreply.github.com>
2021-01-10 13:08:08 +0200

    [BlogCard] - refactored card with material ui and styled-components

--- 9th_task/src/Components/Main/BlogCard.js
+++ 9th_task/src/Components/Main/BlogCard.js
@@ -1,15 +1,32 @@
 import React from "react";
+import Button from '@material-ui/core/Button'
 import { Link } from "react-router-dom";
+import styled from 'styled-components';
+
+
+const BlogCardDiv = styled.div`
+width: 360px;
+margin: 1rem;
+background: #eee;
+padding: 1rem;
+display: inline-block;
+`;
+const BlogCardImg= styled.img`
+width: 100%
+`;
+
 
 const BlogCard = ({ title, author, desc, img, link }) => {
   return (
-    <div className="BlogCard">
+    <BlogCardDiv>
       <h2>{title}</h2>
       <em>{author}</em>
       <p>{desc}</p>
-      <img src={img} alt={title} />
+      <BlogCardImg src={img} alt={title} />
+      <Button variant="contained" color="secondary">
       <Link to={link}>Read more</Link>
-    </div>
+      </Button>
+    </BlogCardDiv>
   );
 };
 
